1f6d0e527ce978afb11d45fd85b82a18df0ce234
yasmine By <benyasminenl@gmail.com>
2020-05-31 15:34:50 +0200

    Added repository

--- Individual assignment/app/src/main/java/com/example/individualassignment/database/CustomPrayerRepository.kt
+++ Individual assignment/app/src/main/java/com/example/individualassignment/database/CustomPrayerRepository.kt
@@ -1,9 +1,30 @@
 package com.example.individualassignment.database
 
 import android.content.Context
+import androidx.lifecycle.LiveData
+import com.example.individualassignment.model.CustomPrayer
 
 class CustomPrayerRepository(context: Context) {
 
-//    var customPrayerDao: CustomPrayerDao
+    var customPrayerDao: CustomPrayerDao
+
+    init {
+        val database =
+            CustomPrayerRoomDatabase.getDatabase(context)
+        customPrayerDao = database!!.customPrayerDao()
+    }
+
+
+    fun getAllCustomPrayers() : LiveData<List<CustomPrayer>> {
+        return customPrayerDao.getAllCustomPrayers()
+    }
+
+
+
+    suspend fun insertCustomPrayer(customPrayer: CustomPrayer)= customPrayerDao.insertCustomPrayer(customPrayer)
+    suspend fun deleteCustomPrayer(customPrayer: CustomPrayer) = customPrayerDao.deleteCustomPrayer(customPrayer)
+    suspend fun deleteAllCustomPrayers()= customPrayerDao.deleteAllCustomPrayers()
+
+
 
 }
